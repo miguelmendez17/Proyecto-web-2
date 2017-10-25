02552b738258580aa2d9b3b17904b2482722e204
Miguel Mendez <miguel1796@live.com>
2017-10-24 23:11:14 -0600

    24 de octubre

--- PHP/login.php
+++ PHP/login.php
@@ -1,23 +1,31 @@
-<?php 
+<?php
 
 include './connectionPHP.php';
 
 if (isset($_REQUEST['btn-login']))
 {
 	$username=$_REQUEST['fieldUsername'];
-    $password=$_REQUEST['fieldPassword'];
+	$password=$_REQUEST['fieldPassword'];
 
 
     $query = "select nombreDeUsuario,contrasenna from usuarios where nombreDeUsuario='$username'";
-    $result=pg_query($globalConnection,$query);
-    $rows = pg_numrows($result);
+  //  $result=pg_query($globalConnection,$query);
+  //  $rows = pg_numrows($result);
 
-
-    echo $result;
-  //  $respuesta=  json_encode(pg_fetch_all($result));
-   
-//    echo ($respuesta);
-
+	  $result=pg_query($conn, $query);
+	  if  (!$result) {
+	   echo "query did not execute";
+	  }
+	  if ($line = pg_fetch_assoc($result)) {
+	  	if ($line['rows'] == 0) {
+	    	echo "0 records";
+	    }
+	  }
+	  else {
+	   while ($row = pg_fetch_array($result)) {
+	   		echo $row;
+	   }
+	  }
 }
 
  ?>
