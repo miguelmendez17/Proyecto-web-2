5acd460d7e68db03b6235638b0c6a482a01041ee
Miguel Mendez <miguel1796@live.com>
2017-10-25 00:05:10 -0600

    Login funcionando, trabajar nada mas menuprincipal.html

--- PHP/login.php
+++ PHP/login.php
@@ -11,16 +11,30 @@ if (isset($_REQUEST['btn-login']))
     $query = "select nombreDeUsuario,contrasenna from usuarios where nombreDeUsuario='$username'";
 
 	$result = pg_query($globalConnection, $query);
-	if (!$result) {
-	    echo "An error occurred.\n";
-	    exit;
+	
+	$rows = pg_num_rows($result);
+
+	if ($rows==0) {
+	   	echo "<script>";
+        echo "window.location = 'http://localhost/Proyecto Web 2/index.html';";
+        echo "</script>";  
+        return;
 	}
 
 	$arr = pg_fetch_all($result);
-    $test  = 0;
-	$prueba = $arr[0];
 
-	print_r($prueba['nombredeusuario']);
+	if (is_array($arr))
+	{
+    	foreach($arr as $array){
+			if($array['nombredeusuario'] == $username && $array['contrasenna']==$password)
+			{
+				echo "<script>";
+        		echo "window.location = 'http://localhost/Proyecto Web 2/menuPrincipal.html';";
+        		echo "</script>";    
+			}
+		}
 	}
+
+}
 
 ?>
