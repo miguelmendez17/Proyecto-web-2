ce2c0856eab85af29bddcea1ad6c35429a82d659
Kenneth Alvarez <kenneth_alv@hotmail.com>
2017-11-04 13:08:00 -0600

    Agregado campos en blanco para la Seleccion de los Equipos.. FALTA recojer todos los equipos y algunos bugs!

--- JS/showSelectionPath.js
+++ JS/showSelectionPath.js
@@ -13,28 +13,38 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault();
 
+    var id = ev.target.className; // Elemento sobre el que se arrastra
+
+
+    if (id === 'team-selected' || id === 'team-repechaje'){ // pregunta si es un espacio en blanco,
+        var data = ev.dataTransfer.getData("text"); //mueve el equipo e arrastrado
+        ev.target.appendChild(document.getElementById(data)); // lo mete dentro del espacio en blanco
+    }else{//si no intercambie las banderas y el ID
+
+
+
     var elementoArrastrado = $("#"+ ev.dataTransfer.getData("text")); // Elemento arrastrado // Elemento arrastrado
 
-    var idArrastrado = elementoArrastrado.attr('id');
-    var srcAnterior = elementoArrastrado.attr('src'); // obtiene la bandera para
+    var idArrastrado = elementoArrastrado.attr('id');//obtiene el id ( nobmre de equipo)
+    var srcAnterior = elementoArrastrado.attr('src'); // obtiene la bandera
 
-    var idPresente = ev.target.id;
-    var srcPresente = ev.target.src;
+    var idPresente = ev.target.id;//obtiene el id (nombre de equipo) del equipo al que se va a drop
+    var srcPresente = ev.target.src; //lo mismo pero con la bandera
 
 
 
 //            alert(srcPresente);
-
+//se asigna los datos del equipo arrastrado al equipo donde se va a drop
     ev.target.src = srcAnterior;
     ev.target.id = idArrastrado;
 
-
+//se asigna los datos del equipo donde se drop
     elementoArrastrado.attr('src',srcPresente);
     elementoArrastrado.attr('id',idPresente);
 
     //  ev.target.appendChild(movecarclone);
     // Añade el elemento arrastrado al elemento desde el que se llama a esta funcion
-
+    }
 
 }
 
@@ -48,7 +58,7 @@ function loadTEAMSNewTour() {
         $('#'+team+'').empty();
 
 
-
+// se encarga de agregar los equipos de cada confederacion,
         $.ajax({
             url: "./PHP/getCountriesConfederation.php?",
             type: "post",
